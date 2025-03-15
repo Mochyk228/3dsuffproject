7533bcf8909b2b03abe857abb33481c201f49359
Mochyk228 <muravskiy.bogdan@icloud.com>
2025-03-15 10:39:05 +0000

    End

--- Scripts/sphere_controller.gd
+++ Scripts/sphere_controller.gd
@@ -5,6 +5,7 @@ extends RigidBody3D
 @export var move_speed : float = 8
 
 @onready var camera_3d: Camera3D = $Camera3D
+@export var end_screen: Control
 
 func _ready():
 	remove_child(camera_3d)
@@ -19,3 +20,13 @@ func _physics_process(delta):
 	var input = Input.get_vector("left", "right", "backward", "forward")
 	var vec = Vector3(input.x, 0, -input.y) * move_speed
 	apply_force(vec)
+	
+	for body in get_colliding_bodies():
+		if body.is_in_group("End"):
+			end_screen.visible = true
+			Engine.time_scale = 0
+
+func _input(event):
+	if event.is_action_pressed("space"):
+		Engine.time_scale = 1
+		get_tree().reload_current_scene()
